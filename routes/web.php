e4a01478c671ecf7f9617603291839ccd0f84a45
awaludin16 <awal72304@gmail.com>
2025-05-19 19:51:11 +0700

    menambahkan template admin

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,19 @@
 <?php
 
+use App\Http\Controllers\Kasir;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Kasir\OrderController;
-use App\Http\Controllers\Owner\ReportController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-Route::middleware(['auth', 'role:cashier'])->group(function () {
-    Route::resource('/kasir/pesanan', OrderController::class);
+    Route::get('/dashboard', [Kasir\DashboardController::class, 'index'])->name('dashboard');
 });
-
-Route::middleware(['auth', 'role:owner'])->group(function () {
-    Route::get('/owner/laporan', [ReportController::class, 'index'])->name('owner.laporan');
-});
-
 
 require __DIR__ . '/auth.php';
